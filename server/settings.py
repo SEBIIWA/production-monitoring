73ead5b13111f95598115b92b527a87ed1a0b348
Walee Sebiiy <waleesebiiy@gmail.com>
2024-07-10 19:52:52 +0100

    update final fix

--- server/settings.py
+++ server/settings.py
@@ -14,11 +14,7 @@ SECRET_KEY = 'django-insecure-p_7)#1-n)g@yje+*i&t6nrw1$vj7-vddnsrxf5x!h@r65+&vpk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'localhost',
-    'amira-server.onrender.com',
-    '0.0.0.0/0'
-]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -29,12 +25,14 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -106,7 +104,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
