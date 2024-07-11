036cefa6ba5bf170e0d1b934f3ec12c9ec5bdea6
Walee Sebiiy <waleesebiiy@gmail.com>
2024-07-11 22:48:23 +0100

    updated login page

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'keenthemes.com',
+        hostname: 'preview.keenthemes.com',
       },
     ],
   },
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,4 +1,6 @@
 import Link from 'next/link'
+import Head from 'next/head'
+import { Fragment } from 'react'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
 
@@ -19,66 +21,67 @@ export default function Index() {
   })
 
   return (
-    <div className='w-full h-full min-h-dvh grid grid-cols-2 p-6'>
-      <div className='flex items-center justify-center py-12'>
-        <div className='mx-auto grid w-[350px] gap-6'>
-          <div className='grid gap-2 text-center'>
-            <h1 className='text-3xl font-bold'>Login</h1>
-            <p className='text-sm text-balance text-muted-foreground'>Enter your email below to login to your account</p>
-          </div>
-          <Form {...form}>
-            <div className='grid gap-4'>
-              <FormField
-                control={form.control}
-                name='email'
-                render={({ field }) => (
-                  <FormItem>
-                    <div className='grid gap-2'>
-                      <FormLabel htmlFor='email'>Email</FormLabel>
-                      <FormControl>
-                        <Input id='email' type='email' placeholder='m@example.com' required {...field} autoComplete='off' />
-                      </FormControl>
-                    </div>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-
-              <FormField
-                control={form.control}
-                name='password'
-                render={({ field }) => (
-                  <FormItem>
-                    <div className='grid gap-2'>
-                      <FormLabel htmlFor='password'>Password</FormLabel>
-                      <FormControl>
-                        <Input id='password' placeholder='Enter your password' type='password' required {...field} />
-                      </FormControl>
-                    </div>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-
-              <Button type='submit' className='w-full mt-4' onClick={form.handleSubmit((data) => push('/dashboard'))}>
-                Login
-              </Button>
-              <Link href='/forgot-password' className='mx-auto inline-block text-sm underline'>
-                Forgot your password?
-              </Link>
+    <Fragment>
+      <Head>
+        <title>Login | WIP Stock Monitoring</title>
+      </Head>
+      <div className='w-full h-full min-h-dvh grid grid-cols-2'>
+        <div className='flex items-center justify-center py-12'>
+          <div className='mx-auto grid w-[350px] gap-6'>
+            <div className='grid gap-2 text-center'>
+              <h1 className='text-3xl font-bold'>Login</h1>
+              <p className='text-sm text-balance text-muted-foreground'>Enter your email below to login to your account</p>
             </div>
-          </Form>
+            <Form {...form}>
+              <div className='grid gap-4'>
+                <FormField
+                  control={form.control}
+                  name='email'
+                  render={({ field }) => (
+                    <FormItem>
+                      <div className='grid gap-2'>
+                        <FormLabel htmlFor='email'>Email</FormLabel>
+                        <FormControl>
+                          <Input id='email' type='email' placeholder='m@example.com' required {...field} autoComplete='off' />
+                        </FormControl>
+                      </div>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name='password'
+                  render={({ field }) => (
+                    <FormItem>
+                      <div className='grid gap-2'>
+                        <FormLabel htmlFor='password'>Password</FormLabel>
+                        <FormControl>
+                          <Input id='password' placeholder='Enter your password' type='password' required {...field} />
+                        </FormControl>
+                      </div>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <Button type='submit' className='w-full mt-4' onClick={form.handleSubmit((data) => push('/dashboard'))}>
+                  Login
+                </Button>
+                <Link href='/forgot-password' className='mx-auto inline-block text-sm underline'>
+                  Forgot your password?
+                </Link>
+              </div>
+            </Form>
+          </div>
+        </div>
+        <div className='w-full h-full hidden bg-muted lg:flex lg:items-center lg:justify-center lg:flex-col overflow-hidden'>
+          <Image src='https://preview.keenthemes.com/metronic8/react/demo1/media/misc/auth-screens.png' alt='Image' width={700} height={700} className='object-cover mb-20' />
+          <h1 className='text-3xl font-semibold tracking-tight pb-4'>Fast, Efficient and Productive</h1>
+          <p className='max-w-lg text-center text-sm'>Transform Your Workflow with Fast, Efficient, and Productive WIP Stock Monitoring</p>
         </div>
       </div>
-      <div className='hidden bg-muted rounded-xl lg:block overflow-hidden border'>
-        <Image
-          src='https://keenthemes.com/static/metronic/tailwind/dist/assets/media/images/2600x1600/1.png'
-          alt='Image'
-          width='1920'
-          height='1080'
-          className='h-full w-full rounded-xl object-cover border'
-        />
-      </div>
-    </div>
+    </Fragment>
   )
 }
