3f140b33ddd3c24ff56fa72e10d928a629c407cc
Walee Sebiiy <waleesebiiy@gmail.com>
2024-08-22 06:57:55 +0100

    new update

--- service/service/urls.py
+++ service/service/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path, include
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     #    path('admin/', admin.site.urls),
     path('api/', include('products.url')),
     path('api/', include('components.url')),
-    path('api/',include('assembling.url')),
+    path('api/', include('assembling.url')),
     path('api/', include('tasks.url')),
     path('api/', include('users.url')),
     # path('auth/', include('guard.url')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- service/users/models.py
+++ service/users/models.py
@@ -1,12 +1,22 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password
 
 
 class UserModel(models.Model):
     id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=255)
-    username = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    cin = models.CharField(max_length=255, unique=True)
+    telephone = models.CharField(max_length=255, unique=True)
+    username = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
-    role = models.CharField(max_length=255)
+    role = models.CharField(max_length=255, default='EMPLOYEE')
     profile_picture = models.ImageField(upload_to='profiles/', null=True, blank=True)
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        # Check if the password is already hashed (assuming the length of a hashed password is 88)
+        if not self.password.startswith('pbkdf2_') or len(self.password) != 88:
+            self.password = make_password(self.password)
+        super(UserModel, self).save(*args, **kwargs)
