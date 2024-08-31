068254722531d922cbfa6c07de89ccf5a9321f43
Walee Sebiiy <waleesebiiy@gmail.com>
2024-08-31 16:52:49 +0100

    categories

---
+++ service/categories/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class CategoryModel(models.Model):
+    name = models.CharField(max_length=100)
+    icon = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
